542c091630fc1d9d40d14dd6729fc2fab4df92c2
DGAlexandru <DGAlexandru@users.noreply.github.com>
2026-01-19 02:05:02 +0200

    refactor(WebServer): Resource not found - 404 pages

--- backend/lib/webserver/res/404/index.js
+++ backend/lib/webserver/res/404/index.js
@@ -2,7 +2,9 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = {
+    cloudflar3: fs.readFileSync(path.join(__dirname, "cloudflar3.html")).toString(),
+    g00gle: fs.readFileSync(path.join(__dirname, "g00gle.html")).toString(),
     iis: fs.readFileSync(path.join(__dirname, "iis.html")).toString(),
-    oracle: fs.readFileSync(path.join(__dirname, "oracle.html")).toString(),
-    tomcat: fs.readFileSync(path.join(__dirname, "tomcat.html")).toString()
+    oracl3: fs.readFileSync(path.join(__dirname, "oracl3.html")).toString(),
+    t0mcat: fs.readFileSync(path.join(__dirname, "t0mcat.html")).toString()
 };
